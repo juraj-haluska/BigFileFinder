f007a1a8af8c0873db331505d196a2e959e7905c
Juraj Haluska <haluskajuraj0@gmail.com>
2019-08-08 22:16:26 +0200

    add connection logic to finder service to main activity view model

--- app/src/main/java/net/spacive/bigfilefinder/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/net/spacive/bigfilefinder/viewmodel/MainActivityViewModel.java
@@ -1,22 +1,39 @@
 package net.spacive.bigfilefinder.viewmodel;
 
 import android.app.Application;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import net.spacive.bigfilefinder.BigFileFinderApp;
 import net.spacive.bigfilefinder.persistence.DirPathDao;
 import net.spacive.bigfilefinder.persistence.DirPathModel;
+import net.spacive.bigfilefinder.service.ClientContract;
+import net.spacive.bigfilefinder.service.FinderService;
+import net.spacive.bigfilefinder.service.ServiceContract;
+import net.spacive.bigfilefinder.util.SizedSortedSet;
 
+import java.io.File;
 import java.util.List;
 
-public class MainActivityViewModel extends AndroidViewModel {
+public class MainActivityViewModel extends AndroidViewModel implements ClientContract {
+
+    public static final String TAG = MainActivityViewModel.class.getSimpleName();
 
     private DirPathDao dirPathDao;
 
-    private LiveData<List<DirPathModel>> dirPaths;
+    private MutableLiveData<Boolean> finderServiceBound = new MutableLiveData<>();
+
+    private ServiceConnection connection;
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
@@ -26,5 +43,67 @@ public class MainActivityViewModel extends AndroidViewModel {
 
     public LiveData<List<DirPathModel>> getDirPaths() {
         return dirPathDao.getDirPaths();
+    }
+
+    public LiveData<Boolean> isFinderServiceBound() {
+        return finderServiceBound;
+    }
+
+    public void startFinderService(int maxFiles, String[] files) {
+        Intent intent = new Intent(getApplication(), FinderService.class);
+        intent.putExtra(FinderService.INTENT_KEY_FILES, files);
+        intent.putExtra(FinderService.INTENT_KEY_MAX_FILES, maxFiles);
+
+        ContextCompat.startForegroundService(getApplication(), intent);
+    }
+
+    public void bindFinderService() {
+        Intent intent = new Intent(getApplication(), FinderService.class);
+
+        final ClientContract clientContract = this;
+
+        connection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                FinderService.ServiceBinder serviceBinder = ((FinderService.ServiceBinder) iBinder);
+                ServiceContract service = serviceBinder.getServiceContract();
+
+                service.onClientReceived(clientContract);
+
+                if (service.isRunning()) {
+                    finderServiceBound.postValue(true);
+                } else {
+                    finderServiceBound.postValue(false);
+                    getApplication().unbindService(this);
+                }
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                finderServiceBound.postValue(false);
+            }
+        };
+
+        getApplication().bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    public void unbindFinderService() {
+        if (connection != null) {
+            Boolean serviceBound = finderServiceBound.getValue();
+            if (serviceBound != null && serviceBound) {
+                getApplication().unbindService(connection);
+                finderServiceBound.postValue(false);
+            }
+        }
+    }
+
+    @Override
+    public void updateProgress(String fileName) {
+
+    }
+
+    @Override
+    public void onResultsReady(SizedSortedSet<File> sortedSet) {
+        unbindFinderService();
     }
 }
